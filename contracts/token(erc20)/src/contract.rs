fe5c51bde97e1b66ecb65c7c3afcd37bedc969b3
linjay0208 <linjay0208@gmail.com>
2022-04-01 21:54:36 +0800

    token update

--- contracts/token(erc20)/src/contract.rs
+++ contracts/token(erc20)/src/contract.rs
@@ -77,3 +77,90 @@ pub fn execute(
         ExecuteMsg::Burn { amount } => try_burn(deps, env, info, &amount),
     }
 }
+
+#[entry_point]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> Result<Binary, ContractError> {
+    match msg {
+        QueryMsg::Balance { address } => {
+            let address_key = deps.api.addr_validate(&address)?;
+            let balance = read_balance(deps.storage, &address_key)?;
+            let out = to_binary(&BalanceResponse {
+                balance: Uint128::from(balance),
+            })?;
+            Ok(out)
+        }
+        QueryMsg::Allowance { owner, spender } => {
+            let owner_key = deps.api.addr_validate(&owner)?;
+            let spender_key = deps.api.addr_validate(&spender)?;
+            let allowance = read_allowance(deps.storage, &owner_key, &spender_key)?;
+            let out = to_binary(&AllowanceResponse {
+                allowance: Uint128::from(allowance),
+            })?;
+            Ok(out)
+        }
+    }
+}
+
+fn try_transfer(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    recipient: String,
+    amount: &Uint128,
+) -> Result<Response, ContractError> {
+    perform_transfer(
+        deps.storage,
+        &info.sender,
+        &deps.api.addr_validate(recipient.as_str())?,
+        amount.u128(),
+    )?;
+    Ok(Response::new()
+        .add_attribute("action", "transfer")
+        .add_attribute("sender", info.sender)
+        .add_attribute("recipient", recipient))
+}
+
+fn try_transfer_from(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    owner: String,
+    recipient: String,
+    amount: &Uint128,
+) -> Result<Response, ContractError> {
+    let owner_address = deps.api.addr_validate(owner.as_str())?;
+    let recipient_address = deps.api.addr_validate(recipient.as_str())?;
+    let amount_raw = amount.u128();
+
+    let mut allowance = read_allowance(deps.storage, &owner_address, &info.sender)?;
+    if allowance < amount_raw {
+        return Err(ContractError::InsufficientAllowance {
+            allowance,
+            required: amount_raw,
+        });
+    }
+    allowance -= amount_raw;
+    write_allowance(deps.storage, &owner_address, &info.sender, allowance)?;
+    perform_transfer(deps.storage, &owner_address, &recipient_address, amount_raw)?;
+
+    Ok(Response::new()
+        .add_attribute("action", "transfer_from")
+        .add_attribute("spender", &info.sender)
+        .add_attribute("sender", owner)
+        .add_attribute("recipient", recipient))
+}
+
+fn try_approve(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    spender: String,
+    amount: &Uint128,
+) -> Result<Response, ContractError> {
+    let spender_address = deps.api.addr_validate(spender.as_str())?;
+    write_allowance(deps.storage, &info.sender, &spender_address, amount.u128())?;
+    Ok(Response::new()
+        .add_attribute("action", "approve")
+        .add_attribute("owner", info.sender)
+        .add_attribute("spender", spender))
+}

9fb542a2d67dacf05f4bb67f768fdc4c5ea16a2e
nightfury <linjay0208@gmail.com>
2022-04-11 06:41:20 +0800

    token update

--- contracts/token(erc20)/src/contract.rs
+++ contracts/token(erc20)/src/contract.rs
@@ -309,3 +309,49 @@ fn is_valid_symbol(symbol: &str) -> bool {
     }
     true
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::msg::InitialBalance;
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{from_slice, Addr, Env, MessageInfo, Storage, Timestamp, Uint128};
+    use cosmwasm_storage::ReadonlyPrefixedStorage;
+
+    fn mock_env_height(signer: &str, height: u64, time: u64) -> (Env, MessageInfo) {
+        let mut env = mock_env();
+        let info = mock_info(signer, &[]);
+        env.block.height = height;
+        env.block.time = Timestamp::from_seconds(time);
+        (env, info)
+    }
+
+    fn get_constants(storage: &dyn Storage) -> Constants {
+        let config_storage = ReadonlyPrefixedStorage::new(storage, PREFIX_CONFIG);
+        let data = config_storage
+            .get(KEY_CONSTANTS)
+            .expect("no config data stored");
+        from_slice(&data).expect("invalid data")
+    }
+
+    fn get_total_supply(storage: &dyn Storage) -> u128 {
+        let config_storage = ReadonlyPrefixedStorage::new(storage, PREFIX_CONFIG);
+        let data = config_storage
+            .get(KEY_TOTAL_SUPPLY)
+            .expect("no decimals data stored");
+        return bytes_to_u128(&data).unwrap();
+    }
+
+    fn get_balance(storage: &dyn Storage, address: &Addr) -> u128 {
+        let balances_storage = ReadonlyPrefixedStorage::new(storage, PREFIX_BALANCES);
+        return read_u128(&balances_storage, address).unwrap();
+    }
+
+    fn get_allowance(storage: &dyn Storage, owner: &Addr, spender: &Addr) -> u128 {
+        let owner_storage = ReadonlyPrefixedStorage::multilevel(
+            storage,
+            &[PREFIX_ALLOWANCES, owner.as_str().as_bytes()],
+        );
+        return read_u128(&owner_storage, spender).unwrap();
+    }
+}
